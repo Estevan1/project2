7a455282d18deed2ec76c262f1e4ac34fc20abf7
Stacey Drake <staceykdrake@gmail.com>
2019-08-01 19:55:08 -0700

    update

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
@@ -24,3 +25,34 @@ app.listen(PORT, function () {
 
 
 
+=======
+// *********************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var express = require("express");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory to be served
+app.use(express.static("app/public"));
+
+// Routes
+// =============================================================
+require("./app/routes/api-routes.js")(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+>>>>>>> Stashed changes
