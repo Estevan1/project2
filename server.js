87930eeceb49a413718a55dff7c0413f007bbfa5
Stacey Drake <staceykdrake@gmail.com>
2019-08-01 20:23:52 -0700

    modify server.js

--- server.js
+++ server.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser')
-var path = require('path')
-var PORT = process.env.PORT || 8080;
-
-
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json())
-
-app.use(express.static('public'))
-
-require('./routes/htmlRoutes.js')(app);
-require('./routes/apiRoutes.js')(app);
-
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-});
-=======
 // *********************************************************************************
 // Server.js - This file is the initial starting point for the Node/Express server.
 // *********************************************************************************
@@ -51,4 +29,3 @@ app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
 
->>>>>>> 79061d5c2bf2a8b381d73fe46585d8a7ff38024e
